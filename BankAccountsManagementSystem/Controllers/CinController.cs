584ba6185e3419afbbacabfdaa1cb05dcc394f04
Abdelkafi Ahmed <abdelkafiahmed@yahoo.fr>
2015-07-03 10:22:24 +0100

    Initial commit

---
+++ BankAccountsManagementSystem/Controllers/CinController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using BankAccountsManagementSystem.DataAccessLayer;
+
+namespace BankAccountsManagementSystem.Controllers
+{
+    public class CinController : Controller
+    {
+        public ActionResult IsCinAvailable(String cin)
+        {
+            try
+            {
+                using (var db = new BankDbContext())
+                {
+                    try
+                    {
+                        var cCin = db.PersonnesPhysiques.Single(p => p.Cin == cin);
+                        return Json(false, JsonRequestBehavior.AllowGet);
+                    }
+                    catch (Exception)
+                    {
+                        return Json(true, JsonRequestBehavior.AllowGet);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("Error");
+            }
+        }
+
+        public ActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ BankAccountsManagementSystem/Models/Compte.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BankAccountsManagementSystem.Models
+{
+    public enum TypeCompte
+    {
+        Epargne,
+        Courant,
+        Professionnel
+    }
+
+    public class Compte
+    {
+        [Display(Name = "Numéro de compte")]
+        public int CompteId { get; set; }
+
+        [Display(Name = "Client")]
+        public int ClientId { get; set; }
+
+        public TypeCompte? Type { get; set; }
+
+        [Display(Name = "Solde")]
+        public decimal SoldeBase { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Date ouverture")]
+        public DateTime DateOuverture { get; set; }
+
+        public virtual Banque Banque { get; set; }
+        public virtual Client Client { get; set; }
+        public virtual ICollection<Operation> Operations { get; set; }
+        public virtual ICollection<Credit> Credits { get; set; }
+    }
+}
