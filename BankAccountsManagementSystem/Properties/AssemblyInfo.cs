ef06decac021459107a1196f45cd91e2c5f27c57
Abdelkafi Ahmed <abdelkafiahmed@yahoo.fr>
2015-07-03 11:14:00 +0100

    Initial commit

--- BankAccountsManagementSystem/Properties/AssemblyInfo.cs
+++ BankAccountsManagementSystem/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("BankAccountsManagementSystem")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCulture("")] 
 
 // L’affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
 // aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de
